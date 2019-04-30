236943ae5654086f341d11a95cdce3b3f5399b9e
JAMES RITCHIE <b6018048@hallam.shu.ac.uk>
2019-04-30 01:08:39 +0100

    update

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
